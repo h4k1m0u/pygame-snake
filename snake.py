cbe63f65b09311808c08c6d3f7aa1ec9836e2ccb
hakim <hakim@freebsd.hakim.com>
2014-08-05 19:50:01 +0100

    - detect collision with apples

--- snake.py
+++ snake.py
@@ -16,11 +16,9 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Snake Game - Score: %s" % score)
 clock = pygame.time.Clock()
 
-# groups & sprites
+# groups & apple sprite
 sprites_group = pygame.sprite.Group()
-
 apple = Apple()
-sprites_group.add(apple)
 
 # sync movement
 pygame.time.set_timer(TIMER_EVENT, 200)
@@ -49,7 +47,7 @@ while True:
                 snake_coords.insert(0, (snake_head[0] + 1, snake_head[1]))
             elif direction == 'left':
                 snake_coords.insert(0, (snake_head[0] - 1, snake_head[1]))
-            elif direction == 'top':
+            elif direction == 'up':
                 snake_coords.insert(0, (snake_head[0], snake_head[1] - 1))
             elif direction == 'down':
                 snake_coords.insert(0, (snake_head[0], snake_head[1] + 1))
@@ -58,11 +56,23 @@ while True:
     sprites_group.empty()
     window.fill((0, 0, 0))
 
-    # render sprites
-    for i, coord in enumerate(snake_coords):
-        # check for collision between head & apple
+    # render snake sprite
+    snake_head_sprite = Snake_Part(snake_coords[0])
+    sprites_group.add(snake_head_sprite)
+
+    for coord in snake_coords[1:]:
         snake_part = Snake_Part(coord)
         sprites_group.add(snake_part)
+
+    # render apple sprite
+    sprites_group.add(apple)
+
+    # check for collision between head & apple
+    if snake_head_sprite.rect.x == apple.rect.x and snake_head_sprite.rect.y == apple.rect.y:
+        score += 1
+        pygame.display.set_caption("Snake Game - Score: %s" % score)
+        snake_coords.append(snake_coords[-1])
+        apple.update()
 
     sprites_group.draw(window)
 
